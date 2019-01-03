1b79f579bc0fc4497691c9010a594064ca49be45
Zack Huber <zack.huber@gmail.com>
2019-01-03 08:41:57 -0700

    changes to get permissions working

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,26 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+    me(parent, args, ctx, info) {
+        //check if there is a current user ID
+        if (!ctx.request.userId) {
+            return null;
+        }
+        return ctx.db.query.user({
+            where: { id: ctx.request.userId }
+        }, info);
+    },
+    async users(parent, args, ctx, info) {
+        // 0. check if they are logged in
+        // if (!ctx.request.userId) {
+        //     throw new Error("You must be logged in to do that");
+        // }
+        // // 1. check if the user has permissions to query users
+        // hasAtLeastOnePermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+
+        // 2. if they do, query all users
+        return ctx.db.query.users({}, info);
+    }
+};
 
 module.exports = Query;
